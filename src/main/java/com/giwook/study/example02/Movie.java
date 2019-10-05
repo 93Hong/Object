f3eb5d975a252f9baa8e9d76b54de5047c9f3240
93hong <sk2rldnr@gmail.com>
2019-10-05 18:47:40 +0900

    코드재사용에는 상속보다 합성이 더 유연하다 changeDiscountPolicy()

--- src/main/java/com/giwook/study/example02/Movie.java
+++ src/main/java/com/giwook/study/example02/Movie.java
@@ -27,4 +27,8 @@ public class Movie {
 	public Money calculateMovieFee(Screening screening) {
 		return fee.minus(discountPolicy.calculateDiscountAmount(screening));
 	}
+
+	public void changeDiscountPolicy(DiscountPolicy discountPolicy) {
+		this.discountPolicy = discountPolicy;
+	}
 }
--- src/test/java/com/giwook/study/example02/MovieTest.java
+++ src/test/java/com/giwook/study/example02/MovieTest.java
@@ -24,4 +24,17 @@ class MovieTest {
 				new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(11, 59))));
 	}
 
+	@Test
+	void test_changeDiscountPolicy() {
+		Movie avatar = new Movie("아바타",
+			Duration.ofMinutes(120),
+			Money.wons(10000),
+			new AmountDiscountPolicy(Money.wons(800),
+				new SequenceCondition(1),
+				new SequenceCondition(10),
+				new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(11, 59))));
+
+		avatar.changeDiscountPolicy(new PercentDiscountPolicy(0.5, new SequenceCondition(1)));
+	}
+
 }
